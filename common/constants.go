a083373b2e8e2cb69926a22dbf91f8440791594f
Aria-Deep <lsjlhljjyy@gmail.com>
2025-04-18 16:41:45 +0800

    update new model

--- common/constants.go
+++ common/constants.go
@@ -3,7 +3,7 @@ package common
 import "time"
 
 var StartTime = time.Now().Unix() // unit: second
-var Version = "v1.1.2"            // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v1.1.3"            // this hard coding will be replaced automatically when building, no need to manually change
 
 type SGModelInfo struct {
 	Model     string
@@ -17,8 +17,9 @@ var modelRegistry = map[string]SGModelInfo{
 	"claude-3-7-sonnet-extended-thinking": {"claude-3-7-sonnet-extended-thinking", "anthropic::2024-10-22::claude-3-7-sonnet-extended-thinking", 64000},
 	"claude-3-5-sonnet-latest":            {"claude-3-5-sonnet-latest", "anthropic::2024-10-22::claude-3-5-sonnet-latest", 64000},
 	"gpt-4o":                              {"gpt-4o", "openai::2024-02-01::gpt-4o", 64000},
-	"o3-mini-medium":                      {"o3-mini-medium", "openai::2024-02-01::o3-mini-medium", 64000},
-	"o1":                                  {"o1", "openai::2024-02-01::o1", 64000},
+	"gpt-4.1":                             {"gpt-4.1", "openai::2024-02-01::gpt-4.1", 64000},
+	"o3":                                  {"o3", "openai::2024-02-01::o3", 64000},
+	"o4-mini":                             {"o4-mini", "openai::2024-02-01::o4-mini", 64000},
 	"gemini-1.5-pro":                      {"gemini-1.5-pro", "google::v1::gemini-1.5-pro", 64000},
 	"gemini-2.0-pro-exp-02-05":            {"gemini-2.0-pro-exp-02-05", "google::v1::gemini-2.0-pro-exp-02-05", 64000},
 	"claude-3-5-haiku-latest":             {"claude-3-5-haiku-latest", "anthropic::2024-10-22::claude-3-5-haiku-latest", 64000},
